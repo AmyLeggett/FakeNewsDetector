91a3a431bc96ea1bc663d4afc2ee0abb2f6e4f40
AmyLeggett <101567647+AmyLeggett@users.noreply.github.com>
2023-03-25 20:54:15 +0000

    lime added

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from lime.lime_text import LimeTextExplainer
 import gunicorn
 
 app = Flask(__name__)
-
+# 1.21.6
 class_names = ["false", "half-true", "mostly-true", "true", "barely-true", "pants-on-fire"]
 explainer = LimeTextExplainer(class_names=class_names)
 
@@ -28,8 +28,11 @@ def get_array(text):
         value.append(proba.tolist()[0])
         print(value)
     return np.array(value)
+
+
 @app.route('/', methods=["GET", "POST"])
 def home():
+    global text
     if request.method == "POST":
         model = AutoModelForSequenceClassification.from_pretrained("prajjwal1/bert-small")
         tokenizer = AutoTokenizer.from_pretrained("prajjwal1/bert-small")
@@ -42,35 +45,17 @@ def home():
         pred = loaded_model.predict(state)
         print(pred)
         if pred == [0]:
-            #exp = explainer.explain_instance(text, get_array,
-                                             #num_features=5, num_samples=50, labels=[0])
-            #exp = exp.as_html()
-            return render_template("result.html", pred=pred, term='False', color_change='#FF3333')
+            return render_template("result.html", pred=pred, term='False', color_change='#FF3333'), text
         if pred == [1]:
-            exp = explainer.explain_instance(text, get_array,
-                                             num_features=5, num_samples=50, labels=[1])
-            exp = exp.as_html()
-            return render_template("result.html", pred=pred, term='Half-true', color_change='#F1A008', exp=exp)
+            return render_template("result.html", pred=pred, term='Half-true', color_change='#F1A008'), text
         if pred == [2]:
-            exp = explainer.explain_instance(text, get_array,
-                                             num_features=5, num_samples=50, labels=[2])
-            exp = exp.as_html()
-            return render_template("result.html", pred=pred, term='Mostly-true', color_change='#92DC19', exp=exp)
+            return render_template("result.html", pred=pred, term='Mostly-true', color_change='#92DC19'), text
         if pred == [3]:
-            exp = explainer.explain_instance(text, get_array,
-                                             num_features=5, num_samples=50, labels=[3])
-            exp = exp.as_html()
-            return render_template("result.html", pred=pred, term='True', color_change='#66CC00', exp=exp)
+            return render_template("result.html", pred=pred, term='True', color_change='#66CC00'), text
         if pred == [4]:
-            exp = explainer.explain_instance(text, get_array,
-                                             num_features=5, num_samples=50, labels=[4])
-            exp = exp.as_html()
-            return render_template("result.html", pred=pred, term='Barely-true', color_change='#F15208', exp=exp)
+            return render_template("result.html", pred=pred, term='Barely-true', color_change='#F15208'), text
         if pred == [5]:
-            exp = explainer.explain_instance(text, get_array,
-                                             num_features=5, num_samples=50, labels=[5])
-            exp = exp.as_html()
-            return render_template("result.html", pred=pred, term='Liar', color_change='#000000')
+            return render_template("result.html", pred=pred, term='Liar', color_change='#000000'), text
     return render_template("home.html")
 
 
@@ -85,4 +70,7 @@ def about():
 
 @app.route('/exp', methods=["GET", "POST"])
 def exp():
-    return render_template("file.html")
+    exp = explainer.explain_instance(text, get_array,
+                                     num_features=5, num_samples=50, labels=[1])
+    exp = exp.as_html()
+    return render_template("exp.html", exp=exp)
