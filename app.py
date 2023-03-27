4c5c0702d75cdbebde60d1cf2cced99e0deff242
AmyLeggett <101567647+AmyLeggett@users.noreply.github.com>
2023-03-27 22:53:00 +0100

    change to results displayed

--- app.py
+++ app.py
@@ -14,10 +14,12 @@ class_names = ["false", "half-true", "mostly-true", "true", "barely-true", "pant
 explainer = LimeTextExplainer(class_names=class_names)
 app.secret_key = "key"
 
-model = AutoModelForSequenceClassification.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
-tokenizer = AutoTokenizer.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
-filename = 'bert-base-multilingual-uncased-sentiment-6label'
+model = AutoModelForSequenceClassification.from_pretrained("dslim/bert-base-NER")
+tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
+filename = 'bert-base-NER-6label'
 loaded_model = pickle.load(open(filename, 'rb'))
+
+
 def get_array(text):
     value = []
     for i in range(len(text)):
@@ -25,10 +27,7 @@ def get_array(text):
         text[i] = tokenizer.encode(text[i], return_tensors="pt")
         text[i] = model(text[i])
         text[i] = text[i].logits.detach().numpy()
-        pred = loaded_model.predict(text[i])
         proba = loaded_model.predict_proba(text[i])
-        print(proba)
-        print(pred)
         value.append(proba.tolist()[0])
     print(value)
     return np.array(value)
@@ -37,26 +36,11 @@ def get_array(text):
 @app.route('/', methods=["GET", "POST"])
 def home():
     if request.method == "POST":
-        session.pop("text", None)
         text = request.form.get("statement")
-        session['text'] = text
-        state = tokenizer.encode(text.lower(), return_tensors="pt")
-        state = model(state)
-        state = preprocessing.normalize(state.logits.detach().numpy())
-        pred = loaded_model.predict(state)
-        print(pred)
-        if pred == [0]:
-            return render_template("result.html", pred=pred, term='False', color_change='#FF3333')
-        if pred == [1]:
-            return render_template("result.html", pred=pred, term='Half-true', color_change='#F1A008')
-        if pred == [2]:
-            return render_template("result.html", pred=pred, term='Mostly-true', color_change='#92DC19')
-        if pred == [3]:
-            return render_template("result.html", pred=pred, term='True', color_change='#66CC00')
-        if pred == [4]:
-            return render_template("result.html", pred=pred, term='Barely-true', color_change='#F15208')
-        if pred == [5]:
-            return render_template("result.html", pred=pred, term='Liar', color_change='#000000')
+        exp = explainer.explain_instance(text, get_array,
+                                         num_features=5, num_samples=500, labels=[3])
+        exp = exp.as_html()
+        return render_template('exp.html', exp=exp)
     return render_template("home.html")
 
 
@@ -67,14 +51,3 @@ if __name__ == '__main__':
 @app.route('/', methods=["GET", "POST"])
 def about():
     return render_template("home.html")
-
-
-@app.route('/exp', methods=["GET", "POST"])
-def exp():
-    text = session.get('text', None)
-    print(text)
-    exp = explainer.explain_instance(text, get_array,
-                                     num_features=5, num_samples=500,labels=[5])
-    exp = exp.as_html()
-    session.pop("text", None)
-    return render_template("exp.html", exp=exp)
