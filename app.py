1ade423de49ea4731c6c9504ab5028d8db32737d
AmyLeggett <101567647+AmyLeggett@users.noreply.github.com>
2023-04-26 22:32:38 +0100

    predictproba test

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def tokens(text):
     texts = tokenizer.encode(texts, return_tensors="pt")
     texts = model(texts)
     texts = texts.logits.detach().numpy()
-    probs = loaded_model.predict(texts)
+    probs = loaded_model.predict_proba(texts)
     return probs
 # Route for main page
 @app.route('/', methods=["GET", "POST"])
