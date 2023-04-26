e302d22740d098e05c222ff2bee4dddb53cdecbb
AmyLeggett <101567647+AmyLeggett@users.noreply.github.com>
2023-04-26 21:39:15 +0100

    test

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from lime.lime_text import LimeTextExplainer
 
 import torch
+
 app = Flask(__name__)
 # Set random seed to make reproducible
 rng = torch.default_generator
@@ -16,6 +17,8 @@ tokenizer = AutoTokenizer.from_pretrained("muhtasham/bert-small-finetuned-wnut17
 # Load pretrained model
 filename = 'model'
 loaded_model = pickle.load(open(filename, 'rb'))
+
+
 # Computes predicted probabilities for all classes for lime module
 def get_array(text):
     value = []
@@ -27,7 +30,6 @@ def get_array(text):
         text[i] = text[i].logits.detach().numpy()
         # Predict probabilities for encoded text through classifier
         proba = loaded_model.predict_proba(text[i])
-        probs = loaded_model.predict(text[i])
         value.append(proba.tolist()[0])
     return np.array(value)
 
@@ -37,14 +39,14 @@ def get_array(text):
 def home():
     if request.method == "POST":
         # Labels for each class for lime explanations
-        class_names = ["false", "half-true", "mostly-true", "true", "barely-true", "Pants On Fire"]
+        #class_names = ["false", "half-true", "mostly-true", "true", "barely-true", "Pants On Fire"]
         # Initialises lime explainer
-        explainer = LimeTextExplainer(class_names=class_names)
+        #explainer = LimeTextExplainer(class_names=class_names)
         # Gets text entered by user from form
         text = request.form.get("statement")
         # Explain prediction with the top 5 words , 10 samples and display explanation for the top label
-        exp = explainer.explain_instance(text, get_array,
-                                         num_features=5, num_samples=10, top_labels=1)
+        # exp = explainer.explain_instance(text, get_array,
+                                         #num_features=5, num_samples=10, top_labels=1)
         text = ''.join(text)
         # Encode text and put through bert model
         text = tokenizer.encode(text, return_tensors="pt")
@@ -54,8 +56,8 @@ def home():
         print(probs)
         # Gets probabilities for all classes from exp
         # Generates html page to display probabilities
-        exp = exp.as_html(predict_proba=False)
-        return render_template('exp.html', exp=exp)
+        #exp = exp.as_html(predict_proba=False)
+        return render_template('exp.html', term = probs)
     return render_template("home.html")
 
 
