a40ca01f74291253fa4d64dd29153ddada79f170
AmyLeggett <101567647+AmyLeggett@users.noreply.github.com>
2023-04-26 20:48:19 +0100

    test

--- app.py
+++ app.py
@@ -1,34 +1,25 @@
-import numpy as np
-from flask import Flask, render_template
-from flask import request
 import pickle
 
-from torch import argmax
+import numpy as np
+from flask import Flask, request, render_template
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from lime.lime_text import LimeTextExplainer
+
 import torch
-
 app = Flask(__name__)
-# Set random seed to make reproducible
-
-
-# Different numpy version
-# 1.21.6
-# 1.24.2
-# Set random seed to make reproducible
-rng = torch.default_generator
-rng.manual_seed(42)
-# Load model and tokenizer from hugging face
-model = AutoModelForSequenceClassification.from_pretrained("muhtasham/bert-small-finetuned-wnut17-ner")
-tokenizer = AutoTokenizer.from_pretrained("muhtasham/bert-small-finetuned-wnut17-ner")
-# Load pretrained model
-filename = 'model'
-loaded_model = pickle.load(open(filename, 'rb'))
-
 
 # Computes predicted probabilities for all classes for lime module
 def get_array(text):
     value = []
+    # Set random seed to make reproducible
+    rng = torch.default_generator
+    rng.manual_seed(42)
+    # Load model and tokenizer from hugging face
+    model = AutoModelForSequenceClassification.from_pretrained("muhtasham/bert-small-finetuned-wnut17-ner")
+    tokenizer = AutoTokenizer.from_pretrained("muhtasham/bert-small-finetuned-wnut17-ner")
+    # Load pretrained model
+    filename = 'model'
+    loaded_model = pickle.load(open(filename, 'rb'))
     for i in range(len(text)):
         text[i] = ''.join(text[i])
         # Encode text and put through bert model
@@ -47,22 +38,21 @@ def home():
     if request.method == "POST":
         # Labels for each class for lime explanations
         class_names = ["false", "half-true", "mostly-true", "true", "barely-true", "Pants On Fire"]
-        # Gets users text input
-        statement = request.form.get("statement")
-        text = ''.join(statement)
-        # Encode text and put through bert model
-        text = tokenizer.encode(text, return_tensors="pt")
-        text = model(text)
-        text = text.logits.detach().numpy()
-        pred = loaded_model.predict(text)
-        return render_template("exp.html", pred=pred, term='Pants on Fire', color_change='#5A0000')
+        # Initialises lime explainer
+        explainer = LimeTextExplainer(class_names=class_names)
+        # Gets text entered by user from form
+        text = request.form.get("statement")
+        # Explain prediction with the top 5 words , 10 samples and display explanation for the top label
+        exp = explainer.explain_instance(text, get_array,
+                                         num_features=5, num_samples=10, top_labels=1)
+        # Gets probabilities for all classes from exp
+        prob = exp.predict_proba
+        y = list(prob)
+        # Generates html page to display probabilities
+        exp = exp.as_html(predict_proba=False)
+        return render_template('exp.html', exp=exp, y=y)
     return render_template("home.html")
 
 
 if __name__ == '__main__':
     app.run()
-
-
-@app.route('/', methods=["GET", "POST"])
-def about():
-    return render_template("home.html")
