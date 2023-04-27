45bc70b5b6e3b7fef74c62f5950ff362ec1b6eee
AmyLeggett <zqs20yeu@uea.ac.uk>
2023-04-27 09:32:12 +0100

    proba test

--- app.py
+++ app.py
@@ -19,20 +19,6 @@ filename = 'model'
 loaded_model = pickle.load(open(filename, 'rb'))
 
 
-# Computes predicted probabilities for all classes for lime module
-def get_array(text):
-    value = []
-    for i in range(len(text)):
-        text[i] = ''.join(text[i])
-        # Encode text and put through bert model
-        text[i] = tokenizer.encode(text[i], return_tensors="pt")
-        text[i] = model(text[i])
-        text[i] = text[i].logits.detach().numpy()
-        # Predict probabilities for encoded text through classifier
-        proba = loaded_model.predict_proba(text[i])
-        probs = loaded_model.predict(text[i])
-        value.append(proba.tolist()[0])
-    return np.array(value)
 
 # Route for main page
 @app.route('/', methods=["GET", "POST"])
@@ -48,15 +34,7 @@ def home():
         # Gets probabilities for all classes from exp
         # Generates html page to display probabilities
         # Labels for each class for lime explanations
-        class_names = ["false", "half-true", "mostly-true", "true", "barely-true", "Pants On Fire"]
-        # Initialises lime explainer
-        explainer = LimeTextExplainer(class_names=class_names)
-        # Gets text entered by user from form
-        # Explain prediction with the top 5 words , 10 samples and display explanation for the top label
-        exp = explainer.explain_instance(text, get_array,
-                                         num_features=5, num_samples=10, top_labels=1)
-        exp = exp.as_html(predict_proba=False)
-        return render_template('exp.html', exp=exp)
+        return render_template('exp.html', term = probs)
     return render_template("home.html")
 
 
