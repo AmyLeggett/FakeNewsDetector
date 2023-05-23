d254062fcc86dcdde2fe0357ed1f658762a601d5
AmyLeggett <101567647+AmyLeggett@users.noreply.github.com>
2023-05-23 19:29:38 +0100

    Comments added

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import pickle
-
 import numpy as np
 from flask import Flask, request, render_template
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from lime.lime_text import LimeTextExplainer
-
 import torch
 
 app = Flask(__name__)
+
 
 def tokenise(text):
     # Set random seed to make reproducible
@@ -24,8 +23,10 @@ def tokenise(text):
     texts = tokenizer.encode(texts, return_tensors="pt")
     texts = model(texts)
     texts = texts.logits.detach().numpy()
+    # Return prediction
     probs = loaded_model.predict(texts)
     return probs
+
 
 def get_array(text):
     # Set random seed to make reproducible
@@ -46,8 +47,11 @@ def get_array(text):
         text[i] = text[i].logits.detach().numpy()
         # Predict probabilities for encoded text through classifier
         proba = loaded_model.predict_proba(text[i])
+        # Creates an array of probabilities
         value.append(proba.tolist()[0])
     return np.array(value)
+
+
 # Route for main page
 @app.route('/', methods=["GET", "POST"])
 def home():
@@ -57,39 +61,45 @@ def home():
         explainer = LimeTextExplainer(class_names=class_names)
         # Gets users text input
         statement = request.form.get("statement")
+        # Get prediction for given statement
         pred = tokenise(statement)
-        print(pred)
         if pred == [0]:
+            # Generates LIME explanation and renders the webpage
             exp = explainer.explain_instance(statement, get_array,
                                              num_features=5, num_samples=10, labels=[0])
             exp = exp.as_html(predict_proba=False)
             return render_template("exp.html", pred=pred, term='False', color_change='#FF3333', exp=exp)
         if pred == [1]:
+            # Generates LIME explanation and renders the webpage
             exp = explainer.explain_instance(statement, get_array,
                                              num_features=5, num_samples=10, labels=[1])
             exp = exp.as_html(predict_proba=False)
             return render_template("exp.html", pred=pred, term='Half-true', color_change='#FF7C00', exp=exp)
         if pred == [2]:
+            # Generates LIME explanation and renders the webpage
             exp = explainer.explain_instance(statement, get_array,
                                              num_features=5, num_samples=10, labels=[2])
             exp = exp.as_html(predict_proba=False)
             return render_template("exp.html", pred=pred, term='Mostly-true', color_change='#D4FF00', exp=exp)
         if pred == [3]:
+            # Generates LIME explanation and renders the webpage
             exp = explainer.explain_instance(statement, get_array,
                                              num_features=5, num_samples=10, labels=[3])
             exp = exp.as_html(predict_proba=False)
             return render_template("exp.html", pred=pred, term='True', color_change='#66CC00', exp=exp)
         if pred == [4]:
+            # Generates LIME explanation and renders the webpage
             exp = explainer.explain_instance(statement, get_array,
                                              num_features=5, num_samples=10, labels=[4])
             exp = exp.as_html(predict_proba=False)
             return render_template("exp.html", pred=pred, term='Barely-true', color_change='#FF2D00', exp=exp)
         if pred == [5]:
+            # Generates LIME explanation and renders the webpage
             exp = explainer.explain_instance(statement, get_array,
                                              num_features=5, num_samples=10, labels=[5])
             exp = exp.as_html(predict_proba=False)
             return render_template("exp.html", pred=pred, term='Pants on Fire', color_change='#5A0000', exp=exp)
-        return render_template('exp.html', exp=exp)
+        return render_template('exp.html')
     return render_template("home.html")
 
 
